2fafc02c893241740c3d1798fa85331d7236de99
Utkarsh Goel <ugoel911@gmail.com>
2021-11-24 00:25:29 +0530

    smaller frame added for prediction in model test

--- Model_Test.py
+++ Model_Test.py
@@ -17,34 +17,37 @@ sum = 0
 
 while True: 
     ret, frame = cap.read()
-    sq_img = np.array([row[:480] for row in np.asarray(frame)])
-    gray = cv.cvtColor(sq_img, cv.COLOR_BGR2GRAY)
-    small = cv.resize(gray, (48, 48))
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
     faces = face_cascade.detectMultiScale(gray, 1.1, 4)
     for (x, y, w, h) in faces:
-        cv.rectangle(sq_img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-
-    img = np.asarray(small).reshape((48,48,1))
-    pred = model.predict(np.array([img]))[0][0] 
+        cv.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
     display_text = ""
 
-    if pred != 0:
-        count += 1
-        if is_age_range:
-            sum += 5 * pred
-            display_text = "predicted age range is: " + str(round(pred * 5)) + " to " + str((round(pred + 1) * 5))
-            print(display_text)
-        else: 
-            sum += pred
-            display_text = "Predicted age: " + str(round(pred))
-            print(display_text)
-
     if (len(faces) >= 1):
-        sq_img = cv.putText(sq_img, display_text, faces[0][:2], cv.FONT_HERSHEY_COMPLEX, 0.5, (255,0,0), 1)
+        (x, y, w, h) = faces[0]
+        cropped = np.array([row[x + 20: x + w - 20] for row in gray[y + 5: y + h - 5]])
+        small = cv.resize(cropped, (48, 48))
+        # cv.imshow('small ', cropped)
+        img = np.asarray(small).reshape((48,48,1))
+        pred = model.predict(np.array([img]))[0][0] 
 
-    cv.imshow('frame', sq_img)
+        if pred != 0:
+            count += 1
+            if is_age_range:
+                sum += 5 * pred
+                display_text = "predicted age range is: " + str(round(pred * 5)) + " to " + str((round(pred + 1) * 5))
+                print(display_text)
+            else: 
+                sum += pred
+                display_text = "Predicted age: " + str(round(pred))
+                print(display_text)
+        
+        frame = cv.putText(frame, display_text, faces[0][:2], cv.FONT_HERSHEY_COMPLEX, 0.5, (255,0,0), 1)
+
+
+    cv.imshow('frame', frame)
 
     if cv.waitKey(1) & 0xFF == ord('q'):
         break
